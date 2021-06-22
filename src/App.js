afc18345b821a1d5bb8233bb44f0f33cdde71ecc
ademtaparci <ademtaparci@gmail.com>
2021-06-22 23:53:46 +0300

    reactSpring

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashbord from './layouts/Dashbord';
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
+import Navi from './layouts/Navi';
+
+function App() {
+  return (
+    <div className="App">
+      <Navi></Navi>
+      <Container className="main">
+      <Dashbord />
+</Container>
+    </div>
+  );
+}
+
+export default App;
